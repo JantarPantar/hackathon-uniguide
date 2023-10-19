731d3b38faf99a553db5e16df707a0361ffe7666
kodym.norbert <72672709+JantarPantar@users.noreply.github.com>
2023-10-19 09:47:43 +0200

    asd
    sd

---
+++ src/pages/Home.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+import {Routes,Route} from "react-router-dom"
+
+function App() {
+  return (
+    <div className="App">
+      Negr
+    </div>
+  );
+}
+
+export default App;
