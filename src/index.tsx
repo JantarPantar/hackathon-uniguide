4975bdd9de9c529bd7c4333caee02c6fea402efd
kodym.norbert <72672709+JantarPantar@users.noreply.github.com>
2023-10-19 10:05:48 +0200

    router
    Co-Authored-By: Negr <5120036+negr@users.noreply.github.com>

--- src/index.tsx
+++ src/index.tsx
@@ -5,18 +5,28 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Home from "./pages/Home"
+import Guide from "./pages/Guide"
+import Parking from "./pages/Parking"
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
+  <div>
+    <nav>
+  <a href='/pamatky'>Pamatky</a>
+  <a href='/parkovani'>Parkovani</a>
+</nav>
 <React.StrictMode>
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Home />}/>
+          <Route path='/pamatky' element={<Guide />} />
+          <Route path='/parkovani' element={<Parking />} />
         </Routes>
       </BrowserRouter>
   </React.StrictMode>
+  </div>
 );
 
 // If you want to start measuring performance in your app, pass a function
--- src/pages/Parking.tsx
+++ src/pages/Parking.tsx
@@ -1,15 +1,11 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
 import {Routes,Route} from "react-router-dom"
-import Home from "./pages/Home"
-import Guide from "./pages/Guide"
-import Parking from "./pages/Parking"
 
 function App() {
   return (
-    <div>
-test
+    <div className="App">
+      Park
     </div>
   );
 }
